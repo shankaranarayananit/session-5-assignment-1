44f3bc781f48ccead0be2dcd4b706e685ab79b71
shankaranarayananit <shankara.88it@gmail.com>
2016-11-02 01:32:24 +0530

    Add files via upload

---
+++ app_assignment1.js
@@ -0,0 +1,5 @@
+﻿var name = "animesh";
+var age = 27;
+var dateofbirth = "20 - 12 - 1989";
+var placeofbirth = "Newdelhi";
+console.log("The name is " + name + " and age  is " + age + " and Dateofbirth  is " + dateofbirth + " and place of bith is " + placeofbirth);
